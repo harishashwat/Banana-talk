e27f5d0760a105d8d0619e0032b48303672bf5ef
harishashwat <anant49pratap@gmail.com>
2023-11-01 04:34:59 +0530

    input output processing

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-
-
 let btnTranslate = document.querySelector("#submit")
-btnTranslate.addEventListener('click',()=>{
-    console.log("Hare Krishna")
-})
+let textArea = document.querySelector("#text-input")
+let  outputDiv = document.querySelector("#output")
+
+
+
+function clickHandeler(){
+    outputDiv.innerText = "yoyooyoyo: " + textArea.value
+}
+
+btnTranslate.addEventListener('click',clickHandeler)
